e8dc0cccd61e425e9147e645966b5a7e1ae44f4f
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-12-14 21:41:13 -0300

    Criando audio player

--- app/src/main/java/br/com/igti/android/futebolquiz/FutebolQuizActivity.java
+++ app/src/main/java/br/com/igti/android/futebolquiz/FutebolQuizActivity.java
@@ -3,6 +3,7 @@ package br.com.igti.android.futebolquiz;
 import android.animation.Animator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.Menu;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 public class FutebolQuizActivity extends Activity {
 
     private static final String TAG = "FutebolQuizActivity";
+    private static final String PREF_PRIMEIRA_VEZ = "primeiraVez";
 
     private static final String KEY_INDICE = "indice";
 
@@ -90,6 +92,23 @@ public class FutebolQuizActivity extends Activity {
 
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume()");
+
+        boolean primeiraVez = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(PREF_PRIMEIRA_VEZ,true);
+
+        if (primeiraVez) {
+            PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                    .edit()
+                    .putBoolean(PREF_PRIMEIRA_VEZ,false)
+                    .commit();
+            Toast.makeText(this, "Bem-vindo ao FutebolQuiz!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
     protected void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG,"onSavedInstanceState");
@@ -122,12 +141,6 @@ public class FutebolQuizActivity extends Activity {
     protected void onStart() {
         super.onStart();
         Log.d(TAG,"onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG,"onResume()");
     }
 
     @Override
